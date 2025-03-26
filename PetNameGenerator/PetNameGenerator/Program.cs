a7d6cb83fe05da9946c45498b2c56772e10f4605
Elmo <hed-fevaleros@smu.edu.ph>
2025-03-26 16:32:31 +0800

    Enable Cors for React App

--- PetNameGenerator/PetNameGenerator/Program.cs
+++ PetNameGenerator/PetNameGenerator/Program.cs
@@ -7,7 +7,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowReactApp",
+		policy =>
+		{
+			policy.WithOrigins("http://localhost:5174") // React app URL
+				  .AllowAnyMethod()
+				  .AllowAnyOrigin()
+				  .AllowAnyHeader();
+		});
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
